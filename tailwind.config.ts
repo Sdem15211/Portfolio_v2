129ba1f6bd0eeb2847f4235d75ce9af81444decf
Simon Demeulemeester <simondemeulemeester@MacBook-Pro.local>
2024-08-15 14:06:06 +0200

    first commit

---
+++ tailwind.config.ts
@@ -0,0 +1,37 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    fontSize: {
+      heading: "5rem",
+      nav: "2rem",
+      body: "1.25rem",
+      accent: "1.25rem",
+      subheading: "1.5rem",
+      work: "3.5rem",
+      mobheading: "2.25rem",
+      mobnav: "1.25rem",
+      mobbody: "0.75rem",
+      mobaccent: "0.875rem",
+      mobsubheading: "1rem",
+      mobwork: "2.5rem",
+      mdheading: "3rem",
+      mdsubheading: "1.25rem",
+    },
+    letterSpacing: {
+      tight: "-.035em",
+    },
+    colors: {
+      prim: "#E3E1DE",
+      txt: "#000000",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
+export default config;
