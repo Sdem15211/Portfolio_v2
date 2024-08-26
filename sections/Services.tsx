16042b4634ca60a54e08b115c744fda1f5779052
Simon Demeulemeester <simondemeulemeester@MacBook-Pro.local>
2024-08-26 14:53:37 +0200

    remove grid

--- sections/Services.tsx
+++ sections/Services.tsx
@@ -1,5 +1,4 @@
 import { Container } from "@/components/layout/Container";
-import { BackgroundGrid } from "@/components/ui/BackgroundGrid";
 import { motion, useScroll, useTransform } from "framer-motion";
 import Image from "next/image";
 import { useRef } from "react";
@@ -15,20 +14,11 @@ export const Services = () => {
   const rotate = useTransform(scrollYProgress, [0, 1], [0, 1080]);
   const sm = useTransform(scrollYProgress, [0, 1], [0, -400]);
   const lg = useTransform(scrollYProgress, [0, 1], [0, -1400]);
-  const md = useTransform(scrollYProgress, [0, 1], [0, -600]);
+  const md = useTransform(scrollYProgress, [0, 1], [0, -800]);
 
   return (
     <section ref={sectionRef} id="services" className="relative">
-      <motion.div
-        initial={{ opacity: 0 }}
-        whileInView={{ opacity: 0.65 }}
-        viewport={{ margin: "0px 0px -400px 0px" }}
-        transition={{ duration: 4, ease: "easeOut" }}
-        className="sticky top-0"
-      >
-        <BackgroundGrid />
-      </motion.div>
-      <Container className="-mt-[900px]">
+      <Container>
         <motion.div
           initial={{ opacity: 0, y: 25 }}
           whileInView={{ opacity: 1, y: 0 }}
@@ -43,47 +33,45 @@ export const Services = () => {
             Services
           </h2>
         </motion.div>
-        <div className="h-[150vh] w-full relative">
-          <motion.div>
-            <motion.div
-              style={{
-                y: lg,
-              }}
-              className="bg-[#161616] flex flex-col gap-8 p-11 w-[390px] justify-center rounded-3xl absolute shadow-2xl left-0 top-[700px]"
-            >
-              <div className="w-[300px] h-[200px] relative rounded-3xl self-center z-10">
-                <Image
-                  src={`/images/MOCKUPDEVELOPMENT.webp`}
-                  fill
-                  alt="develoment"
-                  className="object-cover rounded-3xl"
-                />
-              </div>
-              <div className="relative z-10">
-                <h3 className="text-prim font-menlo text-[1.75rem] mb-8">
-                  &lt;web development&gt;
-                </h3>
-                <p className="text-prim text-[1rem] leading-6">
-                  I build custom websites that bring your unique brand identity
-                  to life. With smooth animations, high performance and easy
-                  navigation, I create sites that look great and keep your
-                  audience engaged. Offering both custom-coded and{" "}
-                  <span className="underline">Webflow</span> options.
-                </p>
-              </div>
-              <div className="absolute inset-0 w-full h-full opacity-25 rounded-3xl z-0">
-                <Image
-                  src={`/images/noise.png`}
-                  fill
-                  alt="noise"
-                  className="rounded-3xl"
-                />
-              </div>
-            </motion.div>
+        <div className="h-[135vh] w-full relative">
+          <motion.div
+            style={{
+              y: lg,
+            }}
+            className="bg-[#161616] flex flex-col gap-8 p-11 w-[390px] justify-center rounded-3xl absolute shadow-2xl left-0"
+          >
+            <div className="w-[300px] h-[200px] relative rounded-3xl self-center z-10">
+              <Image
+                src={`/images/MOCKUPDEVELOPMENT.webp`}
+                fill
+                alt="develoment"
+                className="object-cover rounded-3xl"
+              />
+            </div>
+            <div className="relative z-10">
+              <h3 className="text-prim font-menlo text-[1.75rem] mb-8">
+                &lt;web development&gt;
+              </h3>
+              <p className="text-prim text-[1rem] leading-6">
+                I build custom websites that bring your unique brand identity to
+                life. With smooth animations, high performance and easy
+                navigation, I create sites that look great and keep your
+                audience engaged. Offering both custom-coded and{" "}
+                <span className="underline">Webflow</span> options.
+              </p>
+            </div>
+            <div className="absolute inset-0 w-full h-full opacity-25 rounded-3xl z-0">
+              <Image
+                src={`/images/noise.png`}
+                fill
+                alt="noise"
+                className="rounded-3xl"
+              />
+            </div>
           </motion.div>
           <motion.div
             style={{ y: sm }}
-            className="bg-[#161616] flex flex-col gap-8 p-11 w-[390px] justify-center rounded-3xl absolute shadow-2xl right-0 top-[400px]"
+            className="bg-[#161616] flex flex-col gap-8 p-11 w-[390px] justify-center rounded-3xl absolute shadow-2xl right-0"
           >
             <div className="w-[300px] h-[200px] relative rounded-3xl self-center z-10">
               <Image
@@ -114,7 +102,7 @@ export const Services = () => {
           </motion.div>
           <motion.div
             style={{ y: md }}
-            className="bg-[#161616] p-11 rounded-3xl absolute shadow-2xl w-[800px] left-0 top-[1000px]"
+            className="bg-[#161616] p-11 rounded-3xl absolute shadow-2xl w-[800px] left-0 top-[500px]"
           >
             <div className="w-full h-full flex gap-8 justify-between">
               <div className="w-1/2 h-[240px] relative rounded-3xl self-center z-10">
@@ -146,7 +134,6 @@ export const Services = () => {
             </div>
           </motion.div>
         </div>
-        <div className="h-[600px]" />
       </Container>
     </section>
   );
